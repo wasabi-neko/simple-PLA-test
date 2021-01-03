b5b3752604e09373d4db633c3fd4c3a152c4c767
wasabi-neko <wasabi.neko.owo@gmail.com>
2021-01-04 07:55:41 +0800

    add plot drawing

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import numpy
 import myLib
+import matplotlib.pyplot as plt
 
 
 fileName = './test.csv'
@@ -11,10 +12,9 @@ w_withAge = numpy.load('./withAge.npy')
 print(w_withoutAge)
 print(w_withAge)
 
-for dataVec, ans in zip(dataVecList, ansList):
+survivedList = []
 
-    print(dataVec)
-
+for dataVec in dataVecList:
     # dataVec[3] is age
     if (dataVec[3] == -1):
         result = dataVec.dot(w_withoutAge)
@@ -22,11 +22,60 @@ for dataVec, ans in zip(dataVecList, ansList):
         result = dataVec.dot(w_withAge)
 
     if result > 0:
-        ans = 1
+        survivedList.append(1)
+        print(1)
     else:
-        ans = 0
+        survivedList.append(0)
+        print(0)
 
     print(dataVec)
-    print(ans, end='---------\n')
+    print("------------------")
 
-# draw 
+# draw
+
+# SEX
+maleSurvivedCount = 0
+maleTotal = 0
+
+femaleSurvivedCount = 0
+femaleTotal = 0
+
+# AGE
+ageSurvivedList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+ageTotalList = ageSurvivedList.copy()
+
+for dataVec, isSurvived in zip(dataVecList, survivedList):
+    # SEX
+    if dataVec[2] == 1:
+        maleTotal += 1
+        if isSurvived:
+            maleSurvivedCount += 1
+    else:
+        femaleTotal += 1
+        if isSurvived:
+            femaleSurvivedCount += 1
+
+    #AGE
+    if dataVec[3] != -1:
+        index = dataVec % 10
+        ageTotalList[index] += 1
+        if isSurvived:
+            ageSurvivedList[index] += 1
+# end analize
+
+maleSurvivedRate = maleSurvivedCount / maleTotal
+femaleSurvivedRate = femaleSurvivedCount / femaleTotal
+
+plotX = [1, 2]
+plotY = [maleSurvivedRate, femaleSurvivedRate]
+xLabel = ['male', 'female']
+
+plt.bar(plotX, plotY, tick_label=xLabel, color=['blue', 'red'])
+plt.ylabel('survived rate')
+plt.title('relationship between sex and survivedRate')
+plt.show()
+plt.savefig('sex&survivedRate.png')
+
+
+
+
--- trainAge.py
+++ trainAge.py
@@ -4,7 +4,7 @@ import numpy
 trainFileName = './train.csv'
 
 dList, ansList = myLib.dataReader(trainFileName, includeAge=True, cutDataWhichHasnoAge=True, includeCabin=False)
-weightVec = myLib.trainModelWithMatrixMethod(dList, ansList, counterLimit=100000000)
+weightVec = myLib.trainModelWithMatrixMethod(dList, ansList, counterLimit=1000000000)
 print(weightVec)
 
 numpy.save('./withAge.npy', weightVec)
