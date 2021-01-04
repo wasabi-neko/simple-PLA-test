a0fa5b8aea9e0c04383be7375d41a6bee6eb7e1e
wasabi-neko <wasabi.neko.owo@gmail.com>
2021-01-04 09:07:53 +0800

    add adge relation plot

--- predict.py
+++ predict.py
@@ -41,8 +41,10 @@ femaleSurvivedCount = 0
 femaleTotal = 0
 
 # AGE
-ageSurvivedList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-ageTotalList = ageSurvivedList.copy()
+ageSurvivedList = [0] * 8
+ageTotalList = [0] * 8
+ageSurvivedRateList = [0] * 8
+
 
 for dataVec, isSurvived in zip(dataVecList, survivedList):
     # SEX
@@ -57,7 +59,8 @@ for dataVec, isSurvived in zip(dataVecList, survivedList):
 
     #AGE
     if dataVec[3] != -1:
-        index = dataVec % 10
+        # print(dataVec[3], dataVec[3] / 10)
+        index = int(numpy.floor(dataVec[3] / 10))
         ageTotalList[index] += 1
         if isSurvived:
             ageSurvivedList[index] += 1
@@ -73,8 +76,26 @@ xLabel = ['male', 'female']
 plt.bar(plotX, plotY, tick_label=xLabel, color=['blue', 'red'])
 plt.ylabel('survived rate')
 plt.title('relationship between sex and survivedRate')
-plt.show()
 plt.savefig('sex&survivedRate.png')
+plt.show()
+
+# AGE
+for i in range(8):
+    if ageTotalList[i] != 0 :
+        ageSurvivedRateList[i] = ageSurvivedList[i] / ageTotalList[i]
+# end cacu
+
+plotX = range(8)
+plotY = ageSurvivedRateList
+xLabel = []
+for i in range(8):
+    xLabel.append("{}0~{}0".format(i, i+1))
+
+plt.bar(plotX, plotY, tick_label=xLabel,color=['aqua'])
+plt.title('relationship between age and survivedRate')
+plt.savefig('age&survivedRate.png')
+plt.show()
+
 
 
 
